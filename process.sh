552c0d56a9edd773481fd26a0d37bf248e2cc138
ugjka <esesmu@protonmail.com>
2021-08-16 20:49:36 +0300

    ugjka.net/camera

---
+++ process.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+shopt -s extglob
+mogrify -strip *.jp?(e)g
+mogrify -resize 1300x1300\> *.jp?(e)g
+mv *.jp?(e)g UGJKA.NET/
+sigal build
+sed -i 's/a href=".\/">/a href="\/">/g' _build/index.html
+find _build \( -name '*.css' -o -name '*.html' -o -name '*.js' -o -name '*.json' -o -name '*.txt' -o -name '*.xml' -o -name '*.yml' \) -exec gzip --best --keep -f {} \;
+rsync -Pvhr --delete _build/  ugjka.net:share/camera/
